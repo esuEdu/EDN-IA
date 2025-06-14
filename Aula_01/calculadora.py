68a92cce1cae5ce85d11f35c4da6e1514e2b10ad
Eduardo R Vieira <eduardovieirayt@gmail.com>
2025-06-13 22:04:46 -0300

    Add calculadora

---
+++ Aula_01/calculadora.py
@@ -0,0 +1,9 @@
+productName = "Cadeira Infantil"
+preco = 12.40
+quant = 3
+
+print("Nome: ", productName)
+print("Preco unitario: ", preco)
+print("Quantidade: ", quant)
+print("Valor total: ", quant * preco)
+
